b6f14f35933f7a0e187ebe6d8d460c7d54fb193f
Michael <165153585+haystarkk@users.noreply.github.com>
2025-05-30 16:55:16 +0300

    Create 1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+// Print the first line: "C is fun"
+console.log("C is fun");
+
+// Print the second line: "Python is cool"
+console.log("Python is cool");
+
+// Print the third line: "JavaScript is amazing"
+// Note: 'var' is not used, adhering to the constraint.
+console.log("JavaScript is amazing");
