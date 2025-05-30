5fe3fd96489e7d9ad7f07562f994a0bc29cef2e3
Michael <165153585+haystarkk@users.noreply.github.com>
2025-05-30 17:02:57 +0300

    Create 4-concat.js

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+// Get the first argument from process.argv (index 2)
+const arg1 = process.argv[2];
+
+// Get the second argument from process.argv (index 3)
+const arg2 = process.argv[3];
+
+// Print the arguments in the specified format.
+// If an argument is undefined (not provided), it will print "undefined" as per the example.
+console.log(`${arg1} is ${arg2}`);
